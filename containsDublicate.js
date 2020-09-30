4a07c5cf862005f257de03194494a63b2d521bcb
Nhan Nguyen <nathan13574@gmail.com>
2020-09-30 18:52:31 -0500

    commit

---
+++ containsDublicate.js
@@ -0,0 +1,13 @@
+function containsDuplicate(nums) {
+    const mySet = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (mySet.has(nums[i])) {
+            return true;
+        }
+
+        mySet.add(nums[i]);
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 0]));
